81625274a1a7e8eb8fd13f28ffa4db5c9eec6d97
jorge <jorgehernadez44@gmail.com>
2023-03-27 16:54:39 -0600

    seguí haciendo el ejercicio 1

--- JS-04 - Arrays and Loops/main.js
+++ JS-04 - Arrays and Loops/main.js
@@ -8,21 +8,24 @@ Personas en el Banco
 |     Juan                  |2
 +---------------------------+
 */ 
- let bancoPersonas=["Sofía","David","Juan"];
+let bancoPersonas=["Sofía","David","Juan"];
 
 console.log(bancoPersonas.length); // Da el número 2 en la consola
 let divAlert = document.getElementById("divAlert")
-/
-
-bancoPersonas.push("sara"); // el push pone el elemento al final de la fila
-
-// console.log(libros.shift()); //lo quita y lo manda hasta abajo 
 
 
+bancoPersonas[3]="sara";
+bancoPersonas[4]="Augustin"; // el push pone el elemento al final de la fila
 
-divAlert.innerHTML = bancoPersonas.length + "Personas";
+// console.log(bancoPersonas.shift()); //lo quita y lo manda hasta abajo 
+
+
+
+divAlert.innerHTML = bancoPersonas.length + "<strong> Personas </strong>" ;
 
 divAlert.innerHTML += "<ol><li>" + bancoPersonas[0]+ "</li>"   
 + "<li>" +bancoPersonas[1]+ "</li>" 
-+ "<li>" +bancoPersonas[2]+ "</li> </ol>";
++ "<li>" +bancoPersonas[2]+ "</li>"
+"<li>" +bancoPersonas[3]+ "</li>"
+"<li>" +bancoPersonas[4]+ "</li> </ol>";
 // se concateno todo dentro el primer divAlert para que agarre el ol
